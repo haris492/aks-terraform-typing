a1ab1825e0c1d99f0f4133cbefedff04023b94a5
haris492 <44946049+haris492@users.noreply.github.com>
2024-10-17 18:12:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,18 @@
+
+
 data "azurerm_key_vault" "example" {
   name                = "aks-kv-devops"
   resource_group_name = "aks-kv-rg"
+}
+
+resource "azurerm_key_vault_access_policy" "example" {
+  key_vault_id = data.azurerm_key_vault.example.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = data.azurerm_client_config.current.object_id
+
+  secret_permissions = [
+    "Get",
+  ]
 }
 
 data "azurerm_key_vault_secret" "sshpubkey" {
